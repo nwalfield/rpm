c3cd2bbbae0954823119a3e50b1452d13fb3e2f5
Neal H. Walfield <neal@pep.foundation>
2022-03-29 13:41:18 +0200

    Before importing a certificate, lint it.
      - When importing a certificate, lint the certificate to show the
        user possible issues.  Fail if the certificate is completely
        unusable.
    
      - Fixes #1974.

--- lib/rpmts.c
+++ lib/rpmts.c
@@ -559,6 +559,7 @@ rpmRC rpmtsImportPubkey(const rpmts ts, const unsigned char * pkt, size_t pktlen
 {
     Header h = NULL;
     rpmRC rc = RPMRC_FAIL;		/* assume failure */
+    char *lints = NULL;
     rpmPubkey pubkey = NULL;
     rpmPubkey *subkeys = NULL;
     int subkeysCount = 0;
@@ -569,6 +570,15 @@ rpmRC rpmtsImportPubkey(const rpmts ts, const unsigned char * pkt, size_t pktlen
 
     if (txn == NULL)
 	return rc;
+
+    rc = pgpPubkeyLint(pkt, pktlen, &lints);
+    if (lints) {
+	fprintf(stderr, "%s\n", lints);
+        free(lints);
+    }
+    if (rc) {
+        goto exit;
+    }
 
     /* XXX keyring wont load if sigcheck disabled, force it temporarily */
     rpmtsSetVSFlags(ts, (oflags & ~RPMVSF_MASK_NOSIGNATURES));
